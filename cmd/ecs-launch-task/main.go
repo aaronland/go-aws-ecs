90ae72a86b4aade0a19e1fc2c4da611433e3d49a
thisisaaronland <devnull@localhost>
2020-09-10 11:51:47 -0700

    update docs; go fmt

--- cmd/ecs-launch-task/main.go
+++ cmd/ecs-launch-task/main.go
@@ -4,35 +4,35 @@ import (
 	"flag"
 	"fmt"
 	"github.com/aaronland/go-aws-ecs"
-	"github.com/sfomuseum/go-flags/multi"	
+	"github.com/sfomuseum/go-flags/multi"
 	"log"
 )
 
 func main() {
 
-	task := flag.String("task", "", "")
-	container := flag.String("container", "", "")
-	cluster := flag.String("cluster", "", "")
-	launch_type := flag.String("launch-type", "", "")
-	public_ip := flag.String("public-ip", "", "")
+	task := flag.String("task", "", "The name (and version) of your ECS task.")
+	container := flag.String("container", "", "The name of your ECS container.")
+	cluster := flag.String("cluster", "", "The name of your ECS cluster.")
+	launch_type := flag.String("launch-type", "", "A valid ECS launch type.")
+	public_ip := flag.String("public-ip", "", "A valid ECS public IP string.")
 
 	var subnets multi.MultiString
 	var security_groups multi.MultiString
-	
-	flag.Var(&subnets, "subnet", "...")
-	flag.Var(&security_groups, "security-group", "...")	
-	
-	dsn := flag.String("dsn", "", "")
+
+	flag.Var(&subnets, "subnet", "One or more subnets to run your ECS task in.")
+	flag.Var(&security_groups, "security-group", "A valid AWS security group to run your task under.")
+
+	dsn := flag.String("dsn", "", "A valid aaronland/go-aws-session DSN string.")
 
 	flag.Parse()
 
 	opts := &ecs.TaskOptions{
-		Task:       *task,
-		Container:  *container,
-		Cluster:    *cluster,
-		LaunchType: *launch_type,
-		PublicIP:   *public_ip,
-		Subnets:	subnets,
+		Task:           *task,
+		Container:      *container,
+		Cluster:        *cluster,
+		LaunchType:     *launch_type,
+		PublicIP:       *public_ip,
+		Subnets:        subnets,
 		SecurityGroups: security_groups,
 	}
 
